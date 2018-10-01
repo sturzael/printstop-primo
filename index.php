78be1a76afc8a959f4d08bfba62c7719bafafd76
sturzael <esturz23@gmail.com>
2018-10-02 11:59:17 +1300

    adding auth function

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
 <?php
-
-define('SHORTINIT', true);
-
 include('httpful.phar');
 
-$data = file_get_contents("data.json");
-$decodedData = json_decode($data, true);
-$apikey = $decodedData['apikey'];
-echo "$apikey";
 
-function checkMeeting(){
-$uri = "";
-$response = \Httpful\Request::get($uri)
-    ->addHeader('Authorization', "Bearer $apikey")
+function authentication(){
+  $data = file_get_contents("data.json");
+  $decodedData = json_decode($data, true);
+  $applicationId = $decodedData['applicationId'];
+  $email = $decodedData['email'];
+  $password = $decodedData['password'];
+  $uri = "http://online.printstop.co.nz/API/api/authentication/GenerateAPIToken?email=$email&applicationId=$applicationId&password=$password";
+
+  $response = \Httpful\Request::get($uri)
     ->send();
+  $decodedResponse = json_decode($response,true);
+
+  print_r($decodedResponse['Details']['Token']);
 }
 
-
-// function createMeeting(){
-//   $uri = "https://webapi.teamviewer.com/api/v1/meetings/blizz";
-//   $response = \Httpful\Request::post($uri)
-//       ->addHeader('Authorization', "Bearer ".$GLOBALS['apikey'])
-//       ->sendsType(\Httpful\Mime::FORM)
-//       ->withoutStrictSsl()
-//       ->expectsJson()
-//       ->body("subject=".$GLOBALS['subject']."&start=".$GLOBALS['startTime']."&end=".$GLOBALS['endTime'])
-//       ->send();
-// }
-
+authentication();
 ?>
+<!-- Array ( [Status] => Array (
+                            [APIVersion] => 1.0.31.0
+                            [Success] => 1
+                            [Token] => Array (
+                                                [Status] => Array(
+                                                    [Value] => 3
+                                                    [EnumType] => TokenStatusEnum
+                                                 )
+                                                [ExpiresDateTime] => 0001-01-01T00:00:00 [UserID] => 0
+                                            )
+                          )
+        [Details] => Array (
+          [Email] =>
+          [Password] =>
+          [ApplicationId] =>
+          [Token] =>
+          [GeneratedDateTime] => 2018-10-01T21:47:35.657Z
+          [ExpiresDateTime] => 2018-10-02T02:53:05.99Z )
+        ) -->
