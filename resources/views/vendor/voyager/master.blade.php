923e176500b16981716f740028bcbaa0bbf3a3b9
sturzael <esturz23@gmail.com>
2018-11-13 16:12:58 +1300

    webpack elxir styling config / product page layout

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -42,7 +42,7 @@
     @if(!empty(config('voyager.additional_css')))<!-- Additional CSS -->
         @foreach(config('voyager.additional_css') as $css)<link rel="stylesheet" type="text/css" href="{{ asset($css) }}">@endforeach
     @endif
-
+<link href="/css/app.css" rel="stylesheet" type="text/css">
     @yield('head')
 </head>
 
--- resources/views/vendor/voyager/product.blade.php
+++ resources/views/vendor/voyager/product.blade.php
@@ -1,7 +1,12 @@
 @extends('voyager::master')
 
 @section('content')
-    <div class="page-content"style="padding-left:20px;">
+<div class="row">
+  <div class="col-sm-6" id="product_image">
+    <img src="http://localhost:8000/storage/p0rFdVJ8ZUhNpSWyPK60c0bPlD2Lmr8p7HJ3pMG8.jpeg" width="80%">
+  </div>
+  <div class="col-sm-6">
+    <div class="page-content" style="padding-left:20px;">
         @include('voyager::alerts')
         @include('voyager::dimmers')
         <h1>{{$data['title']}}</h1>
@@ -38,4 +43,7 @@
         </form>
 
     </div>
+  </div>
+</div>
+
 @stop
