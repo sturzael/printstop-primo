045db2cd5c8144b38e6476b59d1b7460c64f5fe4
sturzael <esturz23@gmail.com>
2018-11-26 10:46:06 +1300

    Move script - before ajax

--- resources/views/vendor/voyager/product.blade.php
+++ resources/views/vendor/voyager/product.blade.php
@@ -82,31 +82,6 @@
 </div>
 
 <script type="text/javascript">
-  let items = ['Lamination', 'sizes'];
-  localStorage.clear();
-  for (var i = 0; i < items.length; i++) {
-    document.cookie = `item=${items[i]}`;
-    <?php
-       $cookieItem = $_COOKIE['item'];
-       $itemTypefunction = function(){
-         echo eval('$'. $_COOKIE['item'].'Type' . ';');
-       }; ?>
-       localStorage.setItem(`${items[i]}`, JSON.stringify([
-       <?php foreach ($data[$cookieItem] as $itemTypefunction){
-         echo "{";
-                if (isset($itemTypefunction['ID'])) {
-                  echo 'id:'."'".$itemTypefunction['ID']."'";
-                };
-                if (isset($itemTypefunction['Description'])) {
-                  echo 'description:'."'".$itemTypefunction['Description']."'";
-                }
-                if (isset($itemTypefunction['Code'])) {
-                  echo 'code:'."'".$itemTypefunction['Code']."'";
-                }
-                 echo "},";
-        };?>
-       ]));
 
-  };
 </script>
 @stop
